60aa027e30b784b1593e0b1015f74eef67caf232
c4vxl <manage@c4vxl.de>
2026-05-14 11:28:56 +0200

    Added auto queuing

--- src/main/kotlin/io/github/mayo8432/luckypillars/Main.kt
+++ src/main/kotlin/io/github/mayo8432/luckypillars/Main.kt
@@ -1,10 +1,13 @@
 package io.github.mayo8432.luckypillars
 
+import de.c4vxl.gamemanager.language.Language
+import de.c4vxl.gamemanager.utils.ResourceUtils
 import dev.jorel.commandapi.CommandAPI
 import dev.jorel.commandapi.CommandAPIPaperConfig
 import io.github.mayo8432.luckypillars.commands.TestCommands
 import io.github.mayo8432.luckypillars.game.GameHandler
 import io.github.mayo8432.luckypillars.handler.MovementHandler
+import io.github.mayo8432.luckypillars.handler.QueueHandler
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
@@ -30,9 +33,20 @@ class Main : JavaPlugin() {
         // Save config
         saveResource("allowedmaterials.yml", false)
 
+        // Register language extensions
+        ResourceUtils.readResource("langs", Main::class.java).split("\n")
+            .forEach { langName ->
+                Language.provideLanguageExtension(
+                    "lucky-pillars",
+                    langName,
+                    ResourceUtils.readResource("lang/$langName.yml", Main::class.java)
+                )
+            }
+
         // Introducing Listeners
         GameHandler()
         MovementHandler()
+        QueueHandler()
 
         // Introducing Commands
         // TestCommands()
---
+++ src/main/kotlin/io/github/mayo8432/luckypillars/handler/QueueHandler.kt
@@ -0,0 +1,62 @@
+package io.github.mayo8432.luckypillars.handler
+
+import de.c4vxl.gamemanager.gma.GMA
+import de.c4vxl.gamemanager.gma.event.player.GamePlayerQuitEvent
+import de.c4vxl.gamemanager.gma.event.player.GamePlayerSpectateEndEvent
+import de.c4vxl.gamemanager.gma.player.GMAPlayer
+import de.c4vxl.gamemanager.gma.player.GMAPlayer.Companion.gma
+import io.github.mayo8432.luckypillars.Main
+import org.bukkit.Bukkit
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
+
+/**
+ * Handles game queuing
+ */
+class QueueHandler : Listener {
+    init {
+        Bukkit.getPluginManager().registerEvents(this, Main.instance)
+
+        var i = 0
+        Bukkit.getScheduler().runTaskTimer(Main.instance, Runnable {
+            GMA.registeredGames.forEach { game ->
+                if (!game.isQueuing)
+                    return@forEach
+
+                game.players.forEach {
+                    val language = it.language.child("lucky-pillars")
+
+                        it.bukkitPlayer.sendActionBar(language.getCmp("queue.msg.queuing",
+                            ".".repeat(i + 1), (game.players.size - 1).toString(), (game.size.maxPlayers - 1).toString()
+                        ))
+                }
+            }
+
+            i = if (i < 2) i+1 else 0
+        }, 0, 20)
+    }
+
+    private fun send(player: GMAPlayer) {
+        Bukkit.getScheduler().callSyncMethod(Main.instance) {
+            player.join(GMA.getOrCreate(12, 1))
+        }
+    }
+
+    @EventHandler
+    fun onJoin(event: PlayerJoinEvent) {
+        Bukkit.getScheduler().runTaskLater(Main.instance, Runnable {
+            send(event.player.gma)
+        }, 10)
+    }
+
+    @EventHandler
+    fun onGameLeave(event: GamePlayerQuitEvent) {
+        send(event.player)
+    }
+
+    @EventHandler
+    fun onSpectatorLeave(event: GamePlayerSpectateEndEvent) {
+        send(event.player)
+    }
+}
